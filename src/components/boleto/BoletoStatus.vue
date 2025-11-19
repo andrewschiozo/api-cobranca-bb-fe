15f3e727877db9b19f9f8070214aa54ffadcdf48
Andrews Costa Chiozo <andrewschiozo@gmail.com>
2025-11-19 18:12:38 -0300

    feat: badge do status do boleto

---
+++ src/components/boleto/BoletoStatus.vue
@@ -0,0 +1,45 @@
+<template>
+  <span :class="bgColor">
+    <small>{{ props.statusName?.toUpperCase() }}</small>
+  </span>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps({
+  statusName: String,
+})
+
+const bgColor = computed(() => {
+  switch (props.statusName) {
+    case 'pendente':
+      return 'pico-background-yellow-100'
+
+    case 'registrado':
+      return 'pico-background-blue-500'
+
+    case 'pago':
+      return 'pico-background-green-500'
+
+    case 'cancelado':
+      return 'pico-background-red-500'
+
+    default:
+      return 'pico-background-grey-300'
+  }
+})
+</script>
+
+<style scoped>
+span {
+  padding: 0.25rem 0.5rem;
+  width: 120px;
+  display: inline-block;
+  border-radius: 5px;
+  white-space: nowrap;
+  font-size: small;
+  letter-spacing: 0.15rem;
+  font-weight: bold;
+}
+</style>

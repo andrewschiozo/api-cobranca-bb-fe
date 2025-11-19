f69e0983d7ddd8343a605380ceb636790d9c17c7
Andrews Costa Chiozo <andrewschiozo@gmail.com>
2025-11-19 18:11:47 -0300

    feat: incluído Pagador

--- src/types/BoletoType.ts
+++ src/types/BoletoType.ts
@@ -1,7 +1,10 @@
+import type { PagadorType } from './PagadorType'
+
 export interface BoletoType {
   numeroConvenio: number
   dataVencimento: string
   valorTitulo: number
   nossoNumero: number
   status: string
+  pagador: PagadorType
 }
